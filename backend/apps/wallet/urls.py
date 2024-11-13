3610e9f76c752bc3576cedb12909d79f4638a0c7
Klikovskiy <biisky.k@yandex.ru>
2024-11-13 23:22:16 +0700

    - настройка ссылок на генерацию документации.

--- backend/apps/wallet/urls.py
+++ backend/apps/wallet/urls.py
@@ -1,10 +1,28 @@
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from wallet.views import WalletBalanceView, WalletWithdrawView, WalletDepositView, WalletRefundView
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='API для кошелька',
+        default_version='v1',
+        description='API для работы с кошельком пользователя',
+        contact=openapi.Contact(email='contact@espritgames.ru'),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('balance/', WalletBalanceView.as_view(), name='wallet-balance'),
     path('deposit/', WalletDepositView.as_view(), name='wallet-deposit'),
     path('withdraw/', WalletWithdrawView.as_view(), name='wallet-withdraw'),
     path('refund/', WalletRefundView.as_view(), name='wallet-refund'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
